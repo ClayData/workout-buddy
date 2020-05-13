6fa0a2164d1c23e63bbf56e405913139ce2cdd65
Erik Alfvin <erikalfvin@gmail.com>
2020-05-12 19:58:10 -0500

    Added API delete route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,6 +4,9 @@ const workoutController = require("../controllers/workoutController");
 router.route("/api/workouts/:user")
     .get(workoutController.getUserWorkouts)
     .post(workoutController.createWorkout)
+    
+router.route("/api/workouts/:id")
+    .delete(workoutController.deleteWorkout)
 
 router.get("/health", function(req, res) {
     res.send("Hello from the express server!");
