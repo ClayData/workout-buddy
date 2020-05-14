4c1c6a4226b0f5cc99a95bd82bf4214e28dc7682
Erik Alfvin <erikalfvin@gmail.com>
2020-05-14 18:46:43 -0500

    Working db connection and rendering to screen

--- client/src/components/CreateWorkout.js
+++ client/src/components/CreateWorkout.js
@@ -5,9 +5,9 @@ import { Grid, TextField, List, Button } from '@material-ui/core';
 
 function CreateWorkout() {
     
-        const [formObject, setFormObject] = useState({});
-        const [exerciseList, setExerciseList] = useState([]);
-        const [workoutName, setWorkoutName] = useState();
+    const [formObject, setFormObject] = useState({});
+    const [exerciseList, setExerciseList] = useState([]);
+    const [workoutName, setWorkoutName] = useState();
         
 
 
@@ -25,59 +25,59 @@ function CreateWorkout() {
     }
 
     
-        return(
-            <div>
-             <Grid container>
-                    <Grid item>    
-                        <h2>Create New Workout</h2>
-                        <Grid item>
-                            <TextField
-                            label="Workout Name"
-                            onChange={e => setWorkoutName(e.target.value)}
-                            />
-                        </Grid>
-                        <form>
-                            <Grid container direction="row">
-                                <TextField
-                                name="exercise"
-                                id="exercise"
-                                onChange={handleInputChange}
-                                />
-                                <TextField
-                                name="seconds"
-                                type="Number"
-                                className="number"
-                                id="time"
-                                onChange={handleInputChange}
-                                />
-                                <Button 
-                                variant="contained" 
-                                color="primary" 
-                                onClick={handleFormSubmit} 
-                                // disabled={!(formObject.exercise && formObject.seconds)}
-                                type="submit"
-                                >Add Exercise</Button>
-                            </Grid>
-                        </form>
-                        {exerciseList.length ? (
-                        <List>
-                            {exerciseList.map((exercise) => {
-                                return(
-                                    <WorkoutListItem
-                                    text={exercise.exercise + " " + exercise.seconds}
-                                    />
-                                )
-                                }
-                            )}
-                        </List>
-                         ) : (
-                             <h3>No Exercises Added Yet</h3>
-                         )
-                            }
+    return(
+        <div>
+            <Grid container>
+                <Grid item>    
+                    <h2>Create New Workout</h2>
+                    <Grid item>
+                        <TextField
+                        label="Workout Name"
+                        onChange={e => setWorkoutName(e.target.value)}
+                        />
                     </Grid>
+                    <form>
+                        <Grid container direction="row">
+                            <TextField
+                            name="exercise"
+                            id="exercise"
+                            onChange={handleInputChange}
+                            />
+                            <TextField
+                            name="seconds"
+                            type="Number"
+                            className="number"
+                            id="time"
+                            onChange={handleInputChange}
+                            />
+                            <Button 
+                            variant="contained" 
+                            color="primary" 
+                            onClick={handleFormSubmit} 
+                            // disabled={!(formObject.exercise && formObject.seconds)}
+                            type="submit"
+                            >Add Exercise</Button>
+                        </Grid>
+                    </form>
+                    {exerciseList.length ? (
+                    <List>
+                        {exerciseList.map((exercise) => {
+                            return(
+                                <WorkoutListItem
+                                text={exercise.exercise + " " + exercise.seconds}
+                                />
+                            )
+                            }
+                        )}
+                    </List>
+                        ) : (
+                            <h3>No Exercises Added Yet</h3>
+                        )
+                        }
                 </Grid>
-            </div>
-        )
+            </Grid>
+        </div>
+    )
     
 }
 
