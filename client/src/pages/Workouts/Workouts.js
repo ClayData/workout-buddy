061c1da555a3c4fd23a4a65ff2a731778579df74
ClayData <claytonortiz96@gmail.com>
2020-05-13 21:35:58 -0500

    create workout list now displays on screen

--- client/src/pages/Workouts/Workouts.js
+++ client/src/pages/Workouts/Workouts.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import { Grid, Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import InputForm from '../../components/InputForm';
+import CreateWorkout from '../../components/CreateWorkout'
 import WorkoutListItem from '../../components/WorkoutListItem';
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-       
+    number: {
+       width: '100px'
     }
 })) 
 
@@ -16,29 +16,12 @@ const Workouts = () => {
     const classes = useStyles();
     return(
         <div>
-            <Grid container direction="row" justify="space-around" alignItems="center">
-                <Grid item >
+            <Grid container direction="row" justify="space-around" alignItems="centerg">
+            <Grid item >
                     <h2>Completed Workouts</h2>
                     <WorkoutListItem />    
-                </Grid>        
-                <Grid item >    
-                    <h2>Create New Workout</h2>
-                        <InputForm
-                        name="Workout Name"
-                        />
-                        <InputForm
-                        name="Exercise"
-                        />
-                        <InputForm
-                        name="Exercise"
-                        />
-                        <InputForm
-                        name="Exercise"
-                        />
-                        <InputForm
-                        name="Exercise"
-                        />
-                </Grid>
+                </Grid>       
+                <CreateWorkout />
                 <Grid item >
                     <h2>Existing Workouts</h2>
                     <WorkoutListItem />
