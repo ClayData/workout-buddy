c443b9355ee27a833f4901645bd40d204ea308d6
Erik Alfvin <erikalfvin@gmail.com>
2020-05-19 20:12:50 -0500

    Added list of exercises to run workout page

--- client/src/pages/RunWorkout/RunWorkout.js
+++ client/src/pages/RunWorkout/RunWorkout.js
@@ -1,31 +1,36 @@
 import React, { useState, useEffect } from 'react';
-import { Typography } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
 import API from '../../utils/API';
 import { useParams } from "react-router-dom";
 
+function formatTime(seconds) {
+    return
+}
+
 function RunWorkout() {
+    const [workoutTitle, SetWorkoutTitle] = useState(0);
+    const [exercises, SetExercises] = useState([]);
     const [bigTime, SetBigTime] = useState(0);
     const [lilTime, SetLilTime] = useState(0);
     const [workoutIndex, setWorkoutIndex] = useState(0);
 
     let times = []
-    let totalTime;
-    let currentTime;
 
-    function runTimer(){
+    function runTimer() {
         const reducer = (accumulator, currentValue) => accumulator + currentValue;
-        totalTime = times.reduce(reducer)
-        currentTime = times[workoutIndex];
+        let totalTime = times.reduce(reducer)
+        let currentTime = times[workoutIndex];
 
         let timeInterval = setInterval(function() {
             totalTime--;
             currentTime--;
             console.log(currentTime);
+            console.log(totalTime);
             
             if(totalTime === 0){
                 clearInterval(timeInterval)
             }
-            else if(currentTime <= 0) {
+            else if(currentTime === 0) {
                 setWorkoutIndex(workoutIndex + 1);
                 currentTime = times[workoutIndex]
             }
@@ -42,18 +47,23 @@ function RunWorkout() {
             times = res.data[0].exercises.map((exercise) => {
                 return exercise.duration
             });
+            const exerciseList = res.data[0].exercises.map((exercise) => {
+                return exercise
+            });
+            SetExercises(exerciseList);
+            SetWorkoutTitle(res.data[0].title);
         })
     }, [])
 
     return(
         <div>
-            <Typography>
-                {lilTime}
-            </Typography>
-            <Typography>
-                {bigTime}
-            </Typography>
-            <button onClick={runTimer}>Start</button>
+            <h1>{workoutTitle}</h1>
+            <ul>
+                {exercises.map((exercise, i) => {
+                    return <li key={i}>{exercise.exercise}</li>
+                })}
+            </ul>
+            <Button color="secondary" size="large" variant="contained" onClick={runTimer}>Start Workout</Button>
         </div>
     )
 }
